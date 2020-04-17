c0781fe1ea3c3242c9210202ef287d8a25a6639e
gmarretaf <guilhermemachado@ice.ufjf.br>
2020-04-17 10:45:49 -0300

    Criação de uma nova malha test_hex2d.msh

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,18 @@ int main(int argc, char* argv[])
 
     mesh_t* mesh            = MeshGmshReader(argv[1]);
 
+    cout << endl << "OFFSET: ";
+    for(int i = 0 ; i < mesh->offset.size() ; i++)
+        cout << mesh->offset[i] << " ";
+    cout << endl << "CONN ANTES: ";
+    for(int i = 0 ; i < mesh->conn.size() ; i++)
+        cout << mesh->conn[i] << " ";
+    vector<double> newCoord;
     MeshReordering(mesh, RCM);
-
+    cout << endl << "CONN DEPOIS: ";
+    for(int i = 0 ; i < mesh->conn.size() ; i++)
+        cout << mesh->conn[i] << " ";
+        
     mesh_partition_t *parts = MeshPartitionerInternal(mesh, n_part);
  
     MeshVTKWriterInternal(mesh, out, parts->nodal_part, parts->elem_part);
