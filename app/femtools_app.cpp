8d2bb80e45a452cdfe53dba8c8616281feaecabc
Jose Camata <camata@ice.ufjf.br>
2021-12-06 09:06:15 -0300

    modificado CMakefile para detectar o petsc

--- app/femtools_app.cpp
+++ app/femtools_app.cpp
@@ -1,8 +1,14 @@
-
+#include "petsc.h"
 #include "meshtools.h"
-
+static char help[] = "Empty Proglem\n\n";
 
 int main(int argc, char* argv[])
 {
+    PetscErrorCode ierr;
+    ierr = PetscInitialize(&argc,&argv,nullptr,help); CHKERRQ(ierr);
+
+
+    
+    ierr = PetscFinalize();
     return 0;
 }
