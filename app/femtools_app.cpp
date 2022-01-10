9d6abfe7d3b4c9247eae9929151dad2b612ac364
Guilherme <gfarache@versatushpc.com.br>
2022-01-10 18:09:58 -0300

    correção de erros

--- app/femtools_app.cpp
+++ app/femtools_app.cpp
@@ -16,16 +16,18 @@ int main(int argc, char* argv[])
     IS is; // index set
     ierr = PetscInitialize(&argc,&argv,nullptr,help); CHKERRQ(ierr);
     Mesh_partition_t *parts = new Mesh_partition_t();
+    int processor_id, n_processors;
+    Mesh* mesh;
+    ParallelMesh* pmesh;
     
-
-    MPI_Comm_rank(PETSC_COMM_WORLD,&processor_id );
-    MPI_Comm_size(PETSC_COMM_WORLD,&n_processors );
+    MPI_Comm_rank(PETSC_COMM_WORLD,&processor_id);
+    MPI_Comm_size(PETSC_COMM_WORLD,&n_processors);
 
     if(processor_id == 0)
     {
         // Rodando serial ou em paralelo o processo mestre
         // irá ler a malha. 
-        mesh = new Mesh("test2d.msh");
+        mesh = new Mesh("/home/guilherme/local/app/meshtools/msh/quad.msh");
 
         // Aplica a reordenação nodal considerando o algoritmo
         // escolhido pelo usuário
@@ -34,7 +36,7 @@ int main(int argc, char* argv[])
     
         // Se houver mais um processo, o processo mestre irá
         // particionar a malha
-        if(n_processors > 1 ) {
+        if(n_processors > 1) {
             parts->MeshPartitionerInternal(mesh,n_processors);
         }
     }
@@ -44,8 +46,6 @@ int main(int argc, char* argv[])
         // Malha gerada pelo processo mestre é distribuida
         // para os demais processos. 
         pmesh = parts->DistributedMeshInternal(mesh, processor_id, n_processors);
-
-
     }
     // Criar o Sistema de Equações
     //
@@ -55,73 +55,33 @@ int main(int argc, char* argv[])
     // MATRIZ A
     // Vetores b,x
 
-    // Vec x;                                  // numero de nos totais
-    VecCreateMPI(PETSC_COMM_WORLD,PETSC_DECIDE,pmesh->get_n_nodes(), &x);
+    Vec x;                                  // numero de nos totais
+    VecCreateMPI(PETSC_COMM_WORLD, PETSC_DECIDE, pmesh->get_n_nodes(), &x);
     
-    // Intervalo dos indices globais em cada processo
-    int rstart, rend;
-    VecGetOwnershipRange(x,&rstart,&rend);
+    //Intervalo dos indices globais em cada processo
+    // PetscInt rstart, rend;
+    // VecGetOwnershipRange(x, &rstart, &rend);
 
-    std::cout << "processor ID " << processor_id << "Interval [" << rstart <<","<<rend <<"]\n" << std::flush;
+    //std::cout << "processor ID " << processor_id << "Interval [" << rstart <<","<<rend <<"]\n" << std::flush;
 
     // Prencher o vetor:
-    for(int i = 0; i < pmesh->get_n_elements(); i++)
-    {
-        unsigned int  csize = pmesh->getElementConnSize(i);
-        unsigned int* conn = pmesh->getElementConn(i);
-        for(int j = 0; j < csize; j++)
-            VecSetValue(x,gindices[conn[j]], 1.0, ADD_VALUES);
-    }
+    // for(int i = 0; i < pmesh->get_n_elements(); i++)
+    // {
+    //     unsigned int  csize = pmesh->getElementConnSize(i);
+    //     unsigned int* conn = pmesh->getElementConn(i);
+    //     for(int j = 0; j < csize; j++)
+    //         VecSetValue(x,gindices[conn[j]], 1.0, ADD_VALUES);
+    // }
 
-    VecAssemblyBegin(x);
-    VecAssemblyEnd(x);
+    //VecAssemblyBegin(x);
+    //VecAssemblyEnd(x);
 
-    //VecView(x);
+    //VecView(x, v_view);
 
 
-    Mat A;
+    //Mat A;
     // CSR Matriz Esparsa
     //MatCreateAIJ(PETSC_COMM_WORLD, PETSC_DECIDE,PETSC_DECIDE,pmesh->get_n_nodes(),pmesh->get_n_nodes(),10,NULL, 10, NULL, &A);
-/*
-    PetscInt vector_size = 8;
-    if(rank == 0)
-    }
-    else{
-        if(rank == 1){
-            ierr = VecCreateMPI(PETSC_COMM_WORLD, 3, vector_size, &v); CHKERRQ(ierr); // creating a vector with local size 3 and global size 8
-        }
-        else{
-            ierr = VecCreateMPI(PETSC_COMM_WORLD, 2, vector_size, &v); CHKERRQ(ierr); // creating a vector with local size 3 and global size 8
-        }
-    }
-    VecSetValue(v, 5, value, INSERT_VALUES);
-    
-    VecAssemblyBegin(v); // necessario para atualizar o array entre os processos (?)
-    VecAssemblyEnd(v);
-
-
-    //VecView(v, v_view);
-    
-    // testing index set
-    // indices has each position of the array to every processor
-    // indices = [0, 3, 9, 12] -> processor 1 has elements on input 0th to 2th element,
-    //                            processor 2 has elements 3th to 8th element,
-    //                            processor 3 has elements 9th to 12th element
-
-    // to use IS it is necessary pass the input and the indices to mapping all elements
-    PetscInt indices[] = {0, 3, 9, 12}, n = 5;
-    PetscInt input[] = {10, 20}, *output, m = 5;
-    
-    ISCreateGeneral(PETSC_COMM_SELF,n,indices,PETSC_COPY_VALUES,&is);
-
-    ISView(is,PETSC_VIEWER_STDOUT_SELF);
-
-    ISLocalToGlobalMapping mapping;
-
-    // at the beginning it is necessary to create and set options of the mapping
-    ISLocalToGlobalMappingCreate(PETSC_COMM_WORLD,1,n,indices,PETSC_COPY_VALUES,&mapping);
-    ISLocalToGlobalMappingSetFromOptions(mapping);
-*/
 
     ierr = PetscFinalize();CHKERRQ(ierr);
     return 0;
