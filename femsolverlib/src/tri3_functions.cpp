544edc17809e1e0ec264e3ce15a80118b5a48871
Jose Camata <jose.camata@ufjf.br>
2022-09-19 17:37:04 -0300

    add convection diffusion

--- femsolverlib/src/tri3_functions.cpp
+++ femsolverlib/src/tri3_functions.cpp
@@ -71,26 +71,27 @@ void TRI3ComputeFunctions( RealVector q_point, double qw, std::vector<Point> coo
     double dpsi[2][3];
     double J[2][2]    = {{0.0, 0.0}, {0.0, 0.0}};
     double Jinv[2][2] = {{0.0, 0.0}, {0.0, 0.0}};
+    double x[3], y[3];
    
     TRI3Shape(q_point, phi);
     TRI3DShape(q_point,dpsi);
     for(int i=0; i<3; i++)
     {
-        double x = X(i);
-        double y = Y(i);
-        p_gauss(0) += x*phi[i];
-        p_gauss(1) += y*phi[i];
+        x[i] = X(i);
+        y[i] = Y(i);
+        p_gauss(0) += x[i]*phi[i];
+        p_gauss(1) += y[i]*phi[i];
 
-        J[0][0] +=  x*dpsi[0][i];
-        J[0][1] +=  y*dpsi[0][i];
-        J[1][0] +=  x*dpsi[1][i];
-        J[1][1] +=  y*dpsi[1][i];
+        J[0][0] +=  x[i]*dpsi[0][i];
+        J[0][1] +=  y[i]*dpsi[0][i];
+        J[1][0] +=  x[i]*dpsi[1][i];
+        J[1][1] +=  y[i]*dpsi[1][i];
     }
 
     double detJ = J[0][0]*J[1][1] - J[0][1]*J[1][0];
     if(detJ < 0.0)
     {
-        std::cout << "Error: detJ < 0.0\n" << std::endl;
+        std::cout << "Error: detJ < 0.0  -- det = " << detJ << std::endl;
         exit(1);
     }
 
@@ -105,6 +106,5 @@ void TRI3ComputeFunctions( RealVector q_point, double qw, std::vector<Point> coo
         dphi[i](0) = Jinv[0][0]*dpsi[0][i] + Jinv[0][1]*dpsi[1][i]; // dphi_i/dx
         dphi[i](1) = Jinv[1][0]*dpsi[0][i] + Jinv[1][1]*dpsi[1][i]; // dphi_i/dy
     }
-
     JxW = qw*detJ;
 }
