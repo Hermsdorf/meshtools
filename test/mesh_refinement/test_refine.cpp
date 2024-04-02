e87021a5218de1b805cfd80cbbd2a12e315807fb
Jose Camata <jose.camata@ufjf.br>
2024-04-02 17:01:12 -0300

    adicionado arquivo test/mesh_refinement/test_refine.cpp

---
+++ test/mesh_refinement/test_refine.cpp
@@ -0,0 +1,15 @@
+#include "meshtools.h"
+#include "mesh.h"
+
+
+
+int main(int argc, char *argv[])
+{
+    PetscErrorCode ierr;
+
+    Mesh mesh; 
+    //mesh.read("test/mesh_refinement/mesh.msh");
+    //mesh.refine();
+
+    return 0;
+}
