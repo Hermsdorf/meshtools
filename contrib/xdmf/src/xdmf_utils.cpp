a1d9a33049ea0b049e475a881e45d566e56d9b9d
Jose Camata <jose.camata@ufjf.br>
2024-04-04 11:05:49 -0300

    add  new hdf5 interface

---
+++ contrib/xdmf/src/xdmf_utils.cpp
@@ -0,0 +1,177 @@
+
+#include "xdmf_utils.h"
+
+//----------------------------------------------------------------------------
+std::pair<std::string, int>
+xdmf_utils::get_cell_type(const pugi::xml_node& topology_node)
+{
+  assert(topology_node);
+  pugi::xml_attribute type_attr = topology_node.attribute("TopologyType");
+  assert(type_attr);
+
+  const static std::map<std::string, std::pair<std::string, int>> xdmf_to_meshtools
+      = {
+         {"triangle", {"TRI3", 1}},
+         {"tetrahedron", {"TET4", 1}},
+         {"quadrilateral", {"QUAD4", 1}},
+         {"hexahedron", {"HEX8", 1}},
+        };
+
+  // Convert XDMF cell type string to MeshTools cell type string
+  std::string cell_type = type_attr.as_string();
+  std::transform(cell_type.begin(), cell_type.end(), cell_type.begin(),
+                 [](unsigned char c) { return std::tolower(c); });
+  auto it = xdmf_to_meshtools.find(cell_type);
+  if (it == xdmf_to_meshtools.end())
+  {
+    throw std::runtime_error("Cannot recognise cell type. Unknown value: "
+                             + cell_type);
+  }
+
+  return it->second;
+}
+
+
+//----------------------------------------------------------------------------
+std::array<std::string, 2>
+xdmf_utils::get_hdf5_paths(const pugi::xml_node& dataitem_node)
+{
+  // Check that node is a DataItem node
+  assert(dataitem_node);
+  const std::string dataitem_str = "DataItem";
+  if (dataitem_node.name() != dataitem_str)
+  {
+    throw std::runtime_error("Node name is \""
+                             + std::string(dataitem_node.name())
+                             + R"(", expecting "DataItem")");
+  }
+
+  // Check that format is HDF
+  pugi::xml_attribute format_attr = dataitem_node.attribute("Format");
+  assert(format_attr);
+  const std::string format = format_attr.as_string();
+  if (format.compare("HDF") != 0)
+  {
+    throw std::runtime_error("DataItem format \"" + format
+                             + R"(" is not "HDF")");
+  }
+
+  // Get path data
+  pugi::xml_node path_node = dataitem_node.first_child();
+  assert(path_node);
+
+  // Create string from path and trim leading and trailing whitespace
+  std::string path = path_node.text().get();
+  boost::algorithm::trim(path);
+
+  // Split string into file path and HD5 internal path
+  std::vector<std::string> paths;
+  boost::split(paths, path, boost::is_any_of(":"));
+  assert(paths.size() == 2);
+
+  return {{paths[0], paths[1]}};
+}
+
+
+//-----------------------------------------------------------------------------
+std::filesystem::path
+xdmf_utils::get_hdf5_filename(const std::filesystem::path& xdmf_filename)
+{
+  std::filesystem::path p = xdmf_filename;
+  p.replace_extension("h5");
+  if (p.string() == xdmf_filename)
+  {
+    throw std::runtime_error("Cannot deduce name of HDF5 file from XDMF "
+                             "filename. Filename clash. Check XDMF filename");
+  }
+
+  return p;
+}
+
+//-----------------------------------------------------------------------------
+std::vector<std::int64_t>
+xdmf_utils::get_dataset_shape(const pugi::xml_node& dataset_node)
+{
+  // Get Dimensions attribute string
+  assert(dataset_node);
+  pugi::xml_attribute dimensions_attr = dataset_node.attribute("Dimensions");
+
+  // Gets dimensions, if attribute is present
+  std::vector<std::int64_t> dims;
+  if (dimensions_attr)
+  {
+    // Split dimensions string
+    const std::string dims_str = dimensions_attr.as_string();
+    std::vector<std::string> dims_list;
+    boost::split(dims_list, dims_str, boost::is_any_of(" "));
+
+    // Cast dims to integers
+    for (auto d : dims_list)
+      dims.push_back(boost::lexical_cast<std::int64_t>(d));
+  }
+  return dims;
+}
+
+//----------------------------------------------------------------------------
+std::int64_t xdmf_utils::get_num_cells(const pugi::xml_node& topology_node)
+{
+  assert(topology_node);
+
+  // Get number of cells from topology
+  std::int64_t num_cells_topology = -1;
+  pugi::xml_attribute num_cells_attr
+      = topology_node.attribute("NumberOfElements");
+  if (num_cells_attr)
+    num_cells_topology = num_cells_attr.as_llong();
+
+  // Get number of cells from topology dataset
+  pugi::xml_node topology_dataset_node = topology_node.child("DataItem");
+  assert(topology_dataset_node);
+  const std::vector tdims = get_dataset_shape(topology_dataset_node);
+
+  // Check that number of cells can be determined
+  if (tdims.size() != 2 and num_cells_topology == -1)
+    throw std::runtime_error("Cannot determine number of cells in XDMF mesh");
+
+  // Check for consistency if number of cells appears in both the
+  // topology and DataItem nodes
+  if (num_cells_topology != -1 and tdims.size() == 2)
+  {
+    if (num_cells_topology != tdims[0])
+      throw std::runtime_error("Cannot determine number of cells in XDMF mesh");
+  }
+
+  return std::max(num_cells_topology, tdims[0]);
+}
+
+
+// //----------------------------------------------------------------------------
+// std::string xdmf_utils::vtk_cell_type_str(int cell_type,
+//                                           int num_nodes)
+// {
+//   static const std::map<int, std::map<int, std::string>> vtk_map = {
+//       {mesh::CellType::point, {{1, "PolyVertex"}}},
+//       {mesh::CellType::interval, {{2, "PolyLine"}, {3, "Edge_3"}}},
+//       {mesh::CellType::triangle,
+//        {{3, "Triangle"}, {6, "Triangle_6"}, {10, "Triangle_10"}}},
+//       {mesh::CellType::quadrilateral,
+//        {{4, "Quadrilateral"},
+//         {9, "Quadrilateral_9"},
+//         {16, "Quadrilateral_16"}}},
+//       {mesh::CellType::prism, {{6, "Wedge"}}},
+//       {mesh::CellType::tetrahedron,
+//        {{4, "Tetrahedron"}, {10, "Tetrahedron_10"}, {20, "Tetrahedron_20"}}},
+//       {mesh::CellType::hexahedron, {{8, "Hexahedron"}, {27, "Hexahedron_27"}}}};
+
+//   // Get cell family
+//   auto cell = vtk_map.find(cell_type);
+//   if (cell == vtk_map.end())
+//     throw std::runtime_error("Could not find cell type.");
+
+//   // Get cell string
+//   auto cell_str = cell->second.find(num_nodes);
+//   if (cell_str == cell->second.end())
+//     throw std::runtime_error("Could not find VTK string for cell order.");
+
+//   return cell_str->second;
+// }
